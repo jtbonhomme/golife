2da39ad68af0694876b06b3c45c900555ef9f911
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-12-24 10:54:14 +0100

    criter body

---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+var (
+	emptyImage    = ebiten.NewImage(3, 3)
+	emptySubImage = emptyImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
+)
+
+func init() {
+	emptyImage.Fill(color.White)
+}
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+func maxCounter(index int) int {
+	return 128 + (17*index+32)%64
+}
+
+type point struct {
+	X float64
+	Y float64
+}
+
+const PI float64 = math.Pi
+
+func drawCriterBody(screen *ebiten.Image, counter int) {
+	var path vector.Path
+	npoints := 8
+	center := point{X: 150, Y: 150}
+	direction := -PI / 2
+	size := float64(100.0)
+
+	points := make([]point, npoints)
+
+	indexToPoint := func(i int) (float32, float32) {
+		return float32(points[i].X), float32(points[i].Y)
+	}
+
+	//_size := func(counter int) float64 {
+	//	return size + size*0.1*math.Sin(float64(counter)*2*PI/float64(100))
+	//}
+	// Body
+	points[0] = point{X: center.X + size*math.Cos(direction-PI/8), Y: center.Y + size*math.Sin(direction-PI/8)}
+	points[1] = point{X: center.X + size*math.Cos(direction-3*PI/8), Y: center.Y + size*math.Sin(direction-3*PI/8)}
+	points[2] = point{X: center.X + size*math.Cos(direction-5*PI/8), Y: center.Y + size*math.Sin(direction-5*PI/8)}
+	points[3] = point{X: center.X + size*math.Cos(direction-7*PI/8), Y: center.Y + size*math.Sin(direction-7*PI/8)}
+	points[4] = point{X: center.X + size*math.Cos(direction+7*PI/8), Y: center.Y + size*math.Sin(direction+7*PI/8)}
+	points[5] = point{X: center.X + size*math.Cos(direction+5*PI/8), Y: center.Y + size*math.Sin(direction+5*PI/8)}
+	points[6] = point{X: center.X + size*math.Cos(direction+3*PI/8), Y: center.Y + size*math.Sin(direction+3*PI/8)}
+	points[7] = point{X: center.X + size*math.Cos(direction+PI/8), Y: center.Y + size*math.Sin(direction+PI/8)}
+
+	//points[1] = point{X: center.X + _size(counter)*math.Cos(PI/8)*math.Cos(direction-PI/8), Y: center.X + _size(counter)*math.Cos(PI/8)*math.Sin(direction-PI/8)}
+	//points[3] = point{X: center.X + _size(counter)*math.Cos(3*PI/4)*math.Cos(direction+5*PI/8), Y: center.X + _size(counter)*math.Cos(3*PI/4)*math.Sin(direction+5*PI/8)}
+	//points[5] = point{X: center.X + _size(counter)*math.Cos(3*PI/4)*math.Cos(direction-5*PI/8), Y: center.X + _size(counter)*math.Cos(3*PI/4)*math.Sin(direction-5*PI/8)}
+	//points[7] = point{X: center.X + _size(counter)*math.Cos(PI/8)*math.Cos(direction+PI/8), Y: center.X + _size(counter)*math.Cos(PI/8)*math.Sin(direction+PI/8)}
+
+	for i := 0; i < npoints; i++ {
+		if i == 0 {
+			path.MoveTo(indexToPoint(i))
+			continue
+		}
+		cpx0, cpy0 := indexToPoint(i - 1)
+		x, y := indexToPoint(i)
+		cpx1, cpy1 := x, y
+		//cpx0 += 10
+		//cpx1 -= 10
+		//path.CubicTo(cpx0, cpy0, cpx1, cpy1, x, y)
+		path.QuadTo(cpx0, cpy0, cpx1, cpy1)
+	}
+
+	op := &ebiten.DrawTrianglesOptions{
+		FillRule: ebiten.EvenOdd,
+	}
+	vs, is := path.AppendVerticesAndIndicesForFilling(nil, nil)
+	for i := range vs {
+		vs[i].SrcX = 1
+		vs[i].SrcY = 1
+		vs[i].ColorR = 0x22 / float32(0xff)
+		vs[i].ColorG = 0x33 / float32(0xff)
+		vs[i].ColorB = 0x66 / float32(0xff)
+	}
+	screen.DrawTriangles(vs, is, emptySubImage, op)
+}
+
+func drawEyes(screen *ebiten.Image, side, size, bg float64, counter int) {
+	var path vector.Path
+	center := point{X: 150, Y: 150}
+	dist := 75.0
+	direction := -PI / 2
+
+	path.Arc(float32(center.X+dist*math.Cos(direction+side*PI/8)), float32(center.Y+dist*math.Sin(direction+side*PI/8)), float32(size), float32(0), float32(2*PI), vector.Clockwise)
+
+	op := &ebiten.DrawTrianglesOptions{
+		FillRule: ebiten.EvenOdd,
+	}
+	vs, is := path.AppendVerticesAndIndicesForFilling(nil, nil)
+	for i := range vs {
+		vs[i].SrcX = 1
+		vs[i].SrcY = 1
+		vs[i].ColorR = float32(bg) / float32(0xff)
+		vs[i].ColorG = float32(bg) / float32(0xff)
+		vs[i].ColorB = float32(bg) / float32(0xff)
+	}
+	screen.DrawTriangles(vs, is, emptySubImage, op)
+}
+
+func drawCriter(screen *ebiten.Image, counter int) {
+	drawCriterBody(screen, counter)
+	drawEyes(screen, -1, 15, 0xff, counter)
+	drawEyes(screen, -1, 5, 0x00, counter)
+	drawEyes(screen, 1, 15, 0xff, counter)
+	drawEyes(screen, 1, 5, 0x00, counter)
+}
+
+type Game struct {
+	counter int
+}
+
+func (g *Game) Update() error {
+	g.counter++
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.White)
+	drawCriter(screen, g.counter)
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f\nFPS: %0.2f\nCounter: %d", ebiten.CurrentTPS(), ebiten.CurrentFPS(), g.counter))
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	g := &Game{counter: 0}
+
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Vector (Ebiten Demo)")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
